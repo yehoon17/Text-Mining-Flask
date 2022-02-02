a39747d88fb725f0c1c677c6e86391eac10c4d4f
yehoon17 <yehoon17@gmail.com>
2022-02-03 07:38:29 +0900

    Implement database
    Update __init__.py
     - create database with flask_sqlalchemy
    Update models.py
     - class Document
     - class Analysis

---
+++ website/models.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from . import db
+
+class Document(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    media = db.Column(db.String(20), nullable=False)
+    original_article = db.Column(db.String(80), nullable=False)
+    doc_title = db.Column(db.String(30), nullable=False)
+    doc_content = db.Column(db.Text, nullable=False)
+    doc_datetime = db.Column(db.DateTime, nullable=False)
+    like_count = db.Column(db.Integer, nullable=False)
+    doc_url = db.Column(db.String(80), nullable=False)
+    source = db.Column(db.String(30), nullable=False)
+    customer = db.Column(db.String(20), nullable=False)
+    request_seq = db.Column(db.Integer, nullable=False)
+    doc_id = db.Column(db.String(30), nullable=False)
+    doc_id = db.Column(db.String(30), nullable=False)
+    analysis_id = db.Column(db.Integer, db.ForeignKey('analysis.id'),
+        nullable=False)
+
+
+class Analysis(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(20), nullable=False)
+    documents = db.relationship('Document')
