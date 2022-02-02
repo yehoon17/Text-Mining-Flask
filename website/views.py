029bf616a2aaae22fd2a5a8c07ab990a6b05ae3c
yehoon17 <yehoon17@gmail.com>
2022-02-03 07:41:42 +0900

    Update veiws.py
    Convert uploaded json file and save to database

--- website/views.py
+++ website/views.py
@@ -1,7 +1,18 @@
 # -*- coding: utf-8 -*-
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
+from werkzeug.utils import secure_filename
+import json
+from . import db
+from website.models import Analysis, Document
+from datetime import datetime
 
+
+ALLOWED_EXTENSIONS = {"json"}
+
+def allowed_file(filename):
+    return "." in filename and \
+           filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 views = Blueprint("views", __name__)
 
@@ -9,7 +20,32 @@ views = Blueprint("views", __name__)
 def home():
     return render_template("home.html")
 
-    
-@views.route("/upload")
+@views.route("/upload", methods=["GET", "POST"])
 def upload():
+    if request.method == "POST":
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == "":
+            flash('No selected file')
+        if file:
+            if allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                new_analysis = Analysis(filename=filename)
+                db.session.add(new_analysis)
+                db.session.commit()
+                analysis_id = new_analysis.id
+                for line in file:
+                    db.session.add(to_doc(line, analysis_id))
+                db.session.commit()
+                flash('File uploaded')
+            else:
+                flash('not allowed extension')
+
     return render_template("upload.html")
+
+def to_doc(line, analysis_id):
+    data = json.loads(line)
+    data["doc_datetime"] = datetime.strptime(data["doc_datetime"], "%Y-%m-%dT%H:%M:%S")
+    new_document = Document(analysis_id=analysis_id, **data)
+    return new_document
