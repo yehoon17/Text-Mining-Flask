bd844172625df868c436089aae962367711d8add
yehoon17 <yehoon17@gmail.com>
2022-02-04 15:09:07 +0900

    Update views.py
     - Add route to df
     - Add df to database

--- website/views.py
+++ website/views.py
@@ -21,13 +21,20 @@ def documents():
     return render_template("documents.html", analysis=analysis)
 
 
+@views.route("/df")
+def df():
+    df = DocumentFrequency.query.all()
+    n_doc = Document.query.count()
+    return render_template("df.html", df=df, n_doc=n_doc)
+
+
 @views.route("/documents/<id>")
 def view_document(id):
     documents = Document.query.filter_by(analysis_id=id).all()
     return render_template("articles.html", documents=documents)
 
 
-@views.route("tf/<id>")
+@views.route("/tf/<id>")
 def tf(id):
     termfrequency = TermFrequency.query.filter_by(document_id=id).all()
     return render_template("tf.html", tf=termfrequency)
@@ -41,12 +48,16 @@ def upload():
             flash('No selected file')
         if file:
             if allowed_file(file.filename):
+                df_counter = Counter()
                 analysis_id = add_analysis(file)
                 for line in file:
                     document_id = add_document(line, analysis_id)
                     tf = Counter(preprocess(Document.query.filter_by(
                         id=document_id).first().doc_content))
                     add_tf(tf, document_id)
+                    df_counter.update(set(tf))
+                update_df(df_counter)
+                db.session.commit()
                 flash('File uploaded')
             else:
                 flash('not allowed extension')
