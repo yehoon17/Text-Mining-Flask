7805069a247e8df57a1a570785b2fd42b3104022
yehoon17 <yehoon17@gmail.com>
2022-02-05 04:57:10 +0900

    Update api.py

--- website/api.py
+++ website/api.py
@@ -2,8 +2,12 @@
 
 from email import message
 from flask_restful import Api, Resource, abort, fields, marshal_with, reqparse
-from website.models import DocumentFrequency
+from website.models import DocumentFrequency, Document
 import urllib.parse
+from math import log
+from . import db
+from website.utils import preprocess, normalize
+from collections import Counter
 
 docfreq_fields = {
     "id": fields.Integer,
@@ -14,6 +18,11 @@ docfreq_fields = {
 
 df_get_args = reqparse.RequestParser()
 df_get_args.add_argument("term", type=str, required=True)
+
+df_p_args = reqparse.RequestParser()
+df_p_args.add_argument("term", type=str, required=True)
+df_p_args.add_argument("frequency", type=int, required=True)
+df_p_args.add_argument("idf", type=float)
 
 
 class DocFreq(Resource):
@@ -37,12 +46,36 @@ class DocFreq(Resource):
             return abort(404, message=f"{term} does not exist in database")
 
     @marshal_with(docfreq_fields)
-    def put(self, term: str, frequency: int):
-        pass
+    def put(self):
+        args = df_p_args.parse_args()
+        term = urllib.parse.unquote(args["term"])
+        if result := DocumentFrequency.query.filter_by(term=term).first():
+            abort(409, message=f"{term} already exists in database")
+        else:
+            frequency = args["frequency"]
+            idf = args["idf"]
+            if not idf:
+                n_doc = Document.query.count()
+                idf = log(n_doc / (1 + frequency))
+            df = DocumentFrequency(term=term, frequency=frequency, idf=idf)
+            db.session.add(df)
+            db.session.commit()
+            return df, 201
 
     @marshal_with(docfreq_fields)
-    def patch(self, term: str, frequency: int):
-        pass
+    def patch(self):
+        args = df_p_args.parse_args()
+        term = urllib.parse.unquote(args["term"])
+        if result := DocumentFrequency.query.filter_by(term=term).first():
+            result.frequency = args["frequency"]
+            if idf := args["idf"]:
+                result.idf = idf
+
+            db.session.commit()
+            return result
+        else:
+            abort(404, message=f"{term} doesn't exist, cannot update")
+
 
 class DocFreq2(Resource):
     '''powershell에서 명령어 실행
@@ -56,9 +89,39 @@ class DocFreq2(Resource):
         else:
             return abort(404, message=f"{term} does not exist in database")
 
+    @marshal_with(docfreq_fields)
+    def delete(self, term: str):
+        if result := DocumentFrequency.query.filter_by(term=term):
+            result.delete()
+            db.session.commit()
+            return '', 204
+        else:
+            abort(404, message=f"{term} doesn't exist, cannot delete")
+
+
+tfidf_args = reqparse.RequestParser()
+tfidf_args.add_argument("text", type=str, required=True)
+
+class TFIDF(Resource):
+    def get(self):
+        text = urllib.parse.unquote(tfidf_args.parse_args()["text"])
+        text = preprocess(text)
+        tf = Counter(text)
+        tfidf = []
+        for term in tf:
+            if df := DocumentFrequency.query.filter_by(term=term).first():
+                idf = df.idf
+            else:
+                idf =  log((Document.query.count() + 1) / 2)
+            tfidf.append([term, idf * tf[term]])
+        tfidf = normalize(tfidf, 100)
+        tfidf.sort(key=lambda x: x[1], reverse=True)
+        return tfidf
+
 
 def api(app):
     api_ = Api(app)
 
     api_.add_resource(DocFreq, "/api/df")
     api_.add_resource(DocFreq2, "/api/df/<string:term>")
+    api_.add_resource(TFIDF, "/tfidf")
